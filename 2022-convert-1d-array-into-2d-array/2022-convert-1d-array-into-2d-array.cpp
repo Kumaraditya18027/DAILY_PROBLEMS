abfff276f6067b0dbfcf831c67e1c7bee5950b69
Kumar Aditya <kumaraditya18027@gmail.com>
2024-09-01 06:25:14 +0530

    Time: 87 ms (39.1%), Space: 100.2 MB (5.68%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         vector<vector<int>>matrix;
-        if(n*m<original.size())return {};
+        if(n*m!=original.size())return {};
         int k=0;
         for(int i=0;i<m;i++)
         {
